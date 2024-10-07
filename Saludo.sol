5c7351d74d44e6211630e9bf4c0069694b455509
DemonGor223 <jsbuitragoperdomo@gmail.com>
2024-10-07 18:17:56 -0500

    Creacion de contrato Saludo

---
+++ Saludo.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+contract Saludo {
+
+
+    string saludo = "Saludo inicial desde REMIX";
+
+    function leerSaludo() public view returns (string memory) {
+        return saludo;
+    }
+
+    function guadarSaludo(string memory _nuevoSaludo) public {
+        saludo = _nuevoSaludo;
+    }
+
+
+
+}
